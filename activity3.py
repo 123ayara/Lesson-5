4b8e117d73288891da8c77054278ea068203d1d2
Ayara <sushma.paudel4317@gmail.com>
2025-02-04 17:57:52 -0700

    first commit

---
+++ activity3.py
@@ -0,0 +1,8 @@
+i=int(input("enter a number:"))
+if i<15:
+    print("i is smaller than 15")
+    print("i am an if block")
+else:
+    print("i is greater than 15")
+    print("i am in else block")
+print("i am not an if and not an else block")
---
+++ project.py
@@ -0,0 +1,3 @@
+month=input("Enter the present month:")
+if month=="June" and month=="July" and month=="August" and month=="September":
+    print("Its summertime!")
